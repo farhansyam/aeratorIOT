a4947022be178a025d40b0d00407b20f2ddb7c5d
danang962 <danangmukti@apps.ipb.ac.id>
2022-10-18 16:18:12 +0700

    info sensor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,12 +16,12 @@
             <div class="col s12">
               <div class="card">
                 <div class="card-content">
-                  <h5 class="card-title">Faqs Aerator IOT</h5>
+                  <h5 class="card-title">Faqs AGENS</h5>
                   <br>
                   <ul class="collapsible popout collapsible-dark">
                     <li class="">
                       <div class="collapsible-header" tabindex="0"><i class="material-icons">question_answer</i>Apa itu
-                        Aplikasi Aerator IOT?
+                        Aplikasi AGENS?
                       </div>
                       <div class="collapsible-body" style=""><span>Anim pariatur cliche reprehenderit,
                           enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon
@@ -78,12 +78,12 @@
                       <div class="d-flex align-items-center no-block">
                           <div>
                               <h3 class="font-light">{{$key[$i]}}</h3>
-                              <h6>Terakhir di update : {{$refdetail[$i]['updated_at']}} </h6>
+                              <h6 class="active">Terakhir di update : {{$refdetail[$i]['updated_at']}} </h6>
                           </div>
                           <div class="ml-auto">
                               <div data-label="20%" class="css-bar m-b-0 css-bar-primary css-bar-20"><a href="{{url('dashboard/'.$key[$i])}}"><button class="waves-effect waves-light blue btn"><i class="material-icons dp48">remove_red_eye</i></button></a>
-                              <a href="{{url('kolam/edit/'.$key[$i])}}"><button class="waves-effect waves-light blue btn"><i class="material-icons dp48">edit</i></button></a>
-                              <a href="{{url('kolam/delete/'.$key[$i])}}"><button class="waves-effect waves-light blue btn"><i class="material-icons dp48">delete</i></button></a>
+                              <a href="{{url('kolam/edit/'.$key[$i])}}"><button class="waves-effect waves-light orange btn "><i class="material-icons dp48">edit</i></button></a>
+                              <a href="{{url('kolam/delete/'.$key[$i])}}"><button class="waves-effect waves-light red btn "><i class="material-icons dp48">delete</i></button></a>
                               
                               </div>
                           </div>
@@ -96,7 +96,7 @@
         
           </div>
             <div class="fixed-action-btn">
-              <a class="btn-floating btn-large blue" href="{{url('kolam/create')}}">
+              <a class="btn-floating btn-large blue pulse" href="{{url('kolam/create')}}">
                 <i class="large material-icons">add</i>aa
               </a>
             </div>
