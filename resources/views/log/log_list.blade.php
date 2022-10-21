c654c3db18ed6562d44094f7b6f88f0e949e00b9
farhansyam <31535717+farhansyam@users.noreply.github.com>
2022-10-21 17:31:09 +0700

    gabung

--- resources/views/log/log_list.blade.php
+++ resources/views/log/log_list.blade.php
@@ -23,7 +23,7 @@
                               <h6 class="active">Terakhir di update : {{$refdetail[$i]['updated_at']}} </h6>
                           </div>
                           <div class="ml-auto">
-                              <div data-label="20%" class="css-bar m-b-0 css-bar-primary css-bar-20"><a href="{{url('dashboard/'.$key[$i])}}"><button class="waves-effect waves-light green btn"><i class="material-icons dp48">remove_red_eye</i></button></a>
+                              <div data-label="20%" class="css-bar m-b-0 css-bar-primary css-bar-20"><a href="{{url('log/'.$key[$i])}}"><button class="waves-effect waves-light green btn"><i class="material-icons dp48">remove_red_eye</i></button></a>
                               <a href="{{url('kolam/edit/'.$key[$i])}}"><button class="waves-effect waves-light orange btn "><i class="material-icons dp48">edit</i></button></a>
                               <a href="{{url('kolam/delete/'.$key[$i])}}"><button class="waves-effect waves-light red btn "><i class="material-icons dp48">delete</i></button></a>
                               
@@ -41,5 +41,5 @@
               <a class="btn-floating btn-large blue pulse" href="{{url('kolam/create')}}">
                 <i class="large material-icons">add</i>aa
               </a>
-            </div>  
+            </div>    
 @endsection
